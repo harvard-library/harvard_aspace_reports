7791cab94ad897daffd8d1871674c3a739c0dcff
Dave Mayo <dave_mayo@harvard.edu>
2019-07-01 15:11:39 -0400

    Updated to accomodate inability by MySQL to detect trivially derived tables with literals attached

--- backend/model/locations_with_profile_and_repo.rb
+++ backend/model/locations_with_profile_and_repo.rb
@@ -4,7 +4,6 @@ class LocationsWithProfileAndRepo < AbstractReport
 
   def query_string
     <<~SOME_SQL
-
       SELECT DISTINCT
                 l.id AS locationrecordid,
                 l.building AS locationbuilding,
@@ -26,13 +25,16 @@ class LocationsWithProfileAndRepo < AbstractReport
                 lp.height AS locationheight,
                 lp.width AS locationwidth,
                 lp.depth AS locationdepth,
-                r.collectiontype,
-                REPLACE (r.title,","," ") AS collectiontitle,
-                REPLACE (r.identifier,","," ") AS collectionidentifier,
-                r.eadid,
+                CASE
+                    WHEN COALESCE(r_direct.ead_id, r_by_ao.ead_id, 'None') = 'None' THEN 'accession'
+                    ELSE 'resource'
+                END as collectiontype,
+                REPLACE (COALESCE(r_direct.title, r_by_ao.title, acc.title),","," ") AS collectiontitle,
+                REPLACE (COALESCE(r_direct.identifier, r_by_ao.identifier, acc.identifier),","," ") AS collectionidentifier,
+                COALESCE(r_direct.ead_id, r_by_ao.ead_id, 'None') as eadid,
                 tc.id AS containerrecordid,
                 ev1.value AS containertype,
-                CONCAT("'",tc.indicator) AS containerindicator,
+                CONCAT("'",tc.indicator, "'") AS containerindicator,
                 tc.barcode AS containerbarcode,
                 tc.ils_holding_id AS containerilsholdingid,
                 tc.ils_item_id AS containerilsitemid,
@@ -59,23 +61,13 @@ class LocationsWithProfileAndRepo < AbstractReport
       LEFT JOIN container_profile cp ON tcpr.container_profile_id = cp.id
       LEFT JOIN enumeration_value ev2 ON cp.dimension_units_id = ev2.id
       LEFT JOIN top_container_link_rlshp tcl ON tc.id = tcl.top_container_id
-      LEFT JOIN sub_container sc ON tcl.sub_container_id = sc.id
-      LEFT JOIN (SELECT      i.*,
-                             COALESCE(r1.ead_id, r2.ead_id, acc.ead_id) AS eadid,
-                             COALESCE(r1.title, r2.title, acc.title) AS title,
-                             COALESCE(r1.identifier, r2.identifier, acc.identifier) AS identifier,
-                             COALESCE(r1.collectiontype, r2.collectiontype, acc.collectiontype) AS collectiontype
-                 FROM      instance i
-                 LEFT JOIN archival_object ao
-                        ON i.archival_object_id = ao.id
-                 LEFT JOIN (SELECT id, title, identifier, ead_id, 'resource' as collectiontype FROM resource) r1
-                        ON ao.root_record_id = r1.id
-                 LEFT JOIN (SELECT id, title, identifier, ead_id, 'resource' as collectiontype FROM resource) r2
-                        ON i.resource_id = r2.id
-                 LEFT JOIN (SELECT id, title, identifier, 'None' as ead_id, 'accession' as collectiontype FROM accession) acc
-                        ON i.accession_id = acc.id) r
-             ON sc.instance_id = r.id
-      WHERE (tc.repo_id = #{db.literal(@repo_id)} OR tc.repo_id IS NULL)  AND tch.end_date IS NULL;
+      LEFT JOIN sub_container sc  ON tcl.top_container_id = tc.id AND tcl.sub_container_id = sc.id
+      LEFT JOIN instance i ON i.id = sc.instance_id
+      LEFT JOIN resource r_direct ON i.resource_id = r_direct.id
+      LEFT JOIN archival_object ao ON ao.id = i.archival_object_id
+      LEFT JOIN resource r_by_ao ON ao.root_record_id = r_by_ao.id
+      LEFT JOIN accession acc ON i.accession_id = acc.id
+      WHERE (tc.repo_id = #{db.literal(@repo_id)} OR tc.repo_id IS NULL)  AND tch.end_date IS NULL
 
     SOME_SQL
   end
